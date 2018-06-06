007be07015a30b0452f37a78252ac2d279a05f21
zombieboyh <38679770+zombieboyh@users.noreply.github.com>
2018-06-06 15:12:41 +0800

    3
    摄像机移动

---
+++ youxi/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform Target;
+	private float smooth = 1;
+
+	void Awake()
+	{
+		Target = GameObject.Find ("motobody").transform;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position,new Vector3 (Target.position.x,transform.position.y, transform.position.z), Time.deltaTime * smooth);
+	}
+}
